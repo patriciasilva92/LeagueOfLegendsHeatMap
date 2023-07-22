e8ad33b77c5ed33b97167fafb40f33f69612ffbb
keeplivin <mshaik30@illinois.edu>
2023-07-21 23:57:07 -0500

    api calls function perfectly and data is stored in heatmapdata.txt perfectly

--- leagueCoord.py
+++ leagueCoord.py
@@ -61,38 +61,43 @@ def getPositionData(matchIds, data):
 		# build url for API request
 
 		#print(matchId)
-		url = 'https://americas.api.riotgames.com/lol/match/v5/matches/' + str(matchId) + '?includeTimeline=true&api_key=[RGAPI-4b9da20f-1e5b-4840-a1b7-1d3c65e4a4b6]'
-		#print(url)
-		response = requests.get(url)
+		url = 'https://americas.api.riotgames.com/lol/match/v5/matches/' + str(matchId) +'/timeline'
+		response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+		"Accept-Language": "en-US,en;q=0.8",
+		"Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+		"Origin": "https://developer.riotgames.com",
+		"X-Riot-Token": "RGAPI-4b9da20f-1e5b-4840-a1b7-1d3c65e4a4b6"})
 		response_json = response.json()
 		#data.append('START OF MATCH: ' + str(matchId))
 
 		# Getting errors on certain matches, use try to skip these. Not sure why but error will be given on different matches when script is rerun. 
 		# Might be something to do with API server
-		try: 
+		# print(str(response))
 
-			for frame in response_json['timeline']['frames']:
-	# sometimes the last frame of a game does not contain position data, use try for error checking
-				try:
-					data.append([frame['participantFrames']['1']['position']['x'] , frame['participantFrames']['1']['position']['y']])
-					data.append( [frame['participantFrames']['2']['position']['x'] , frame['participantFrames']['2']['position']['y'] ] )
-					data.append( [frame['participantFrames']['3']['position']['x'] , frame['participantFrames']['3']['position']['y'] ] )
-					data.append( [frame['participantFrames']['4']['position']['x'] , frame['participantFrames']['4']['position']['y'] ] )
-					data.append( [frame['participantFrames']['5']['position']['x'] , frame['participantFrames']['5']['position']['y'] ] )
-					data.append( [frame['participantFrames']['6']['position']['x'] , frame['participantFrames']['6']['position']['y'] ] )
-					data.append( [frame['participantFrames']['7']['position']['x'] , frame['participantFrames']['7']['position']['y'] ] )
-					data.append( [frame['participantFrames']['8']['position']['x'] , frame['participantFrames']['8']['position']['y'] ] )
-					data.append( [frame['participantFrames']['9']['position']['x'] , frame['participantFrames']['9']['position']['y'] ] )
-					data.append( [frame['participantFrames']['10']['position']['x'] , frame['participantFrames']['10']['position']['y'] ] )
-				except KeyError:
-					continue
+		snap = response_json['info']['frames']
+
+		x=0
+		try: 
+				for frame in snap:
+				# sometimes the last frame of a game does not contain position data, use try for error checking
+					try:
+						x=x+1
+						#print(str(x) + '\n')
+						data.append([frame['participantFrames']['1']['position']['x'] , frame['participantFrames']['1']['position']['y']])
+						data.append( [frame['participantFrames']['2']['position']['x'] , frame['participantFrames']['2']['position']['y'] ] )
+						data.append( [frame['participantFrames']['3']['position']['x'] , frame['participantFrames']['3']['position']['y'] ] )
+						data.append( [frame['participantFrames']['4']['position']['x'] , frame['participantFrames']['4']['position']['y'] ] )
+						data.append( [frame['participantFrames']['5']['position']['x'] , frame['participantFrames']['5']['position']['y'] ] )
+						data.append( [frame['participantFrames']['6']['position']['x'] , frame['participantFrames']['6']['position']['y'] ] )
+						data.append( [frame['participantFrames']['7']['position']['x'] , frame['participantFrames']['7']['position']['y'] ] )
+						data.append( [frame['participantFrames']['8']['position']['x'] , frame['participantFrames']['8']['position']['y'] ] )
+						data.append( [frame['participantFrames']['9']['position']['x'] , frame['participantFrames']['9']['position']['y'] ] )
+						data.append( [frame['participantFrames']['10']['position']['x'] , frame['participantFrames']['10']['position']['y'] ] )
+					except KeyError:
+						continue
 
 		except KeyError:
 			continue
-
-			# delay loop so API request limit is not exceeded
-			time.sleep(delay)
-
 	return data
 
 def writeToFile(data):
@@ -118,6 +123,7 @@ def main():
 	print(matchIds)
 	data = []
 	data = getPositionData(matchIds, data)
+	#print(data)
 	writeToFile(data)
 	print("done")
 	
